811d15be23e3db72d473288700cffa6871d02b64
gitahn59 <gooahn59@gmail.com>
2019-10-31 13:57:49 +0900

    Add Main and Controller class
    Main : main thread and user interface class
    Controller : Controller class using AWS SDK

---
+++ AWSController/src/myproject/awscontroller/Controller.java
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : Main.java
+// Author      : gitahn59
+// Version     : 1.0
+// Copyright   : MIT
+// Description : Controller class using AWS SDK
+//============================================================================
+
+package myproject.awscontroller;
+
+public class Controller {
+	
+}
---
+++ AWSController/src/myproject/awscontroller/Main.java
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Main.java
+// Author      : gitahn59
+// Version     : 1.0
+// Copyright   : MIT
+// Description : Main thread and user interface class
+//============================================================================
+
+package myproject.awscontroller;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
