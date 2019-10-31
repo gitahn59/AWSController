1511298893f24a0bd65ed768737aef20e992854e
gitahn59 <gooahn59@gmail.com>
2019-10-31 14:36:38 +0900

    Add AWSController.init()
    initialize method checks a AWS credentials file

--- AWSController/src/myproject/awscontroller/Controller.java
+++ AWSController/src/myproject/awscontroller/Controller.java
@@ -8,6 +8,28 @@
 
 package myproject.awscontroller;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+
 public class Controller {
+	private AmazonEC2 ec2;
 	
+	public void init() throws Exception{
+		ProfileCredentialsProvider profileCredentialsProvider = new ProfileCredentialsProvider();
+		try {
+			profileCredentialsProvider.getCredentials(); // check credentials 
+		}catch(Exception e) {
+			// cannot load credentials 
+			// throws Exception
+			throw new Exception("Cannot load the credentials from the credential profiles file."
+					+"Please make sure that your credentials file is at the correct" 
+					+"location (~/.aws/credentials), and is invalid format.",e);
+		}
+		//initialize AmazomEC2
+		ec2=AmazonEC2ClientBuilder.standard()
+				.withCredentials(profileCredentialsProvider)
+				.withRegion("us-east-1")
+				.build();
+	} 
 }
--- AWSController/src/myproject/awscontroller/Main.java
+++ AWSController/src/myproject/awscontroller/Main.java
@@ -13,6 +13,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Controller controller = new Controller(); // AWS controller 
+		try {
+			controller.init(); // initialize AWS controller
+		}catch(Exception e) { 
+			// There is no credentials file or invalid format
+			System.out.println("Cannot load the credentials from the credential profiles file.");
+			System.out.println("Please make sure that your credentials file is at the correct location (~/.aws/credentials), ");
+			System.out.println("and is invalid format.");
+			return; // halt 
+		}
+		
 		Scanner menu = new Scanner(System.in); 
 		Scanner id_string= new Scanner(System.in); 
 		int menuNum = 0; // menu Number
